68f83de801c63bc6f2715b22b76db4bd4ddb8c32
tim-steel <tim@CrotchBag.(none)>
2015-02-18 20:01:31 +1000

    first function complete

--- cachematrix.R
+++ cachematrix.R
@@ -2,9 +2,29 @@
 ## functions do
 
 ## Write a short comment describing this function
-
+#function to create 4 sub functions returned as a list
+#sub functions are used to interact with scope variables
 makeCacheMatrix <- function(x = matrix()) {
-
+    #Declare inv as Null
+    inv <- NULL
+    set <- function(y) {
+        #Allows new matrix to be set in function
+        #clears previous inv value
+        x <<- y
+        inv <<- NULL
+    } 
+    #allow retrival of matrix
+    get <- function() x
+    #enable setting inverse of x
+    setInv <- function() inv <<-solve(x)
+    #enable retrival of inverse of x
+    getInv <- function() inv
+    
+    #Generate variables accessabe from 
+    # a different environment
+    list(set=set, get=get,
+         setInv=setInv,
+         getInv=getInv)
 }
 
 
@@ -12,4 +32,13 @@ makeCacheMatrix <- function(x = matrix()) {
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+    m <- x$getInv()
+    if(!is.null(m)) {
+        message("getting cached data")
+        return(m)
+    }
+    data <- x$get()
+    m <- solve(data)
+    x$setInv(m)
+    m
 }
